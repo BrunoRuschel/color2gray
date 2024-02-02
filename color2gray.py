073025a5c6d3024d93e49410e2eb63910fae1f54
BrunoRuschel <120383354+BrunoRuschel@users.noreply.github.com>
2024-02-02 18:07:26 -0300

    Update color2gray.py

--- color2gray.py
+++ color2gray.py
@@ -6,7 +6,6 @@ from matplotlib import pyplot as plt
 
 # Carregar a imagem
 input_img = cv2.imread('mapa.png')
-output_img = cv2.imread('mapa.png')
 
 theta = math.radians(45)
 u = 1000
@@ -58,6 +57,13 @@ def delta(i, j, alpha, theta):
 
 print('fez delta ')
 
+# Função para calcular os limites da vizinhança
+def calcula_limites(i, j, u, height, width):
+    viz_cima = max(i - u, 0)
+    viz_baixo = min(i + u, height - 1)
+    viz_esq = max(j - u, 0)
+    viz_dir = min(j + u, width - 1)
+    return viz_cima, viz_baixo, viz_esq, viz_dir
 
 # -------------------calcula a vizinhanca de um pixel de acordo com o parâmetro u---------------------------
 # cria uma lista 2d e inicializa com zeros
@@ -65,22 +71,7 @@ vizinhanca = np.zeros((height, width))
 # garante que a vizinhança está nos limites da imagem
 for i in range(0, height):
     for j in range(0, width):
-        if (i - u) >= 0:
-            viz_cima = i - u
-        else:
-            viz_cima = 0
-        if (i + u) <= (height - 1):
-            viz_baixo = i + u
-        else:
-            viz_baixo = height - 1
-        if (j - u) >= 0:
-            viz_esq = j - u
-        else:
-            viz_esq = 0
-        if (j + u) <= (width - 1):
-            viz_dir = j + u
-        else:
-            viz_dir = width - 1
+        viz_cima, viz_baixo, viz_esq, viz_dir = calcula_limites(i, j, u, height, width)
         for k in range(viz_esq, viz_dir+1):
             for z in range(viz_cima, viz_baixo+1):
                 # garante que o pixel central nao é contado
@@ -89,35 +80,17 @@ for i in range(0, height):
 
 print('vizinhanca ')
 
-# Calculate target difference, where deltas[i][j] stores delta_ij
+# calcula target difference, deltas[i][j] = delta_ij
 deltas = [[0 for _ in range(0, height * width)] for _ in range(0, height * width)]
 for i in range(0, height):
     for j in range(0, width):
-        if (i - u) >= 0:
-            viz_cima = i - u
-        else:
-            viz_cima = 0
-        if (i + u) <= (height - 1):
-            viz_baixo = i + u
-        else:
-            viz_baixo = height - 1
-        if (j - u) >= 0:
-            viz_esq = j - u
-        else:
-            viz_esq = 0
-        if (j + u) <= (width - 1):
-            viz_dir = j + u
-        else:
-            viz_dir = width - 1
+        viz_cima, viz_baixo, viz_esq, viz_dir = calcula_limites(i, j, u, height, width)
         for k in range(viz_cima, viz_baixo+1):
             for z in range(viz_esq, viz_dir+1):
                 deltas[i * width + j][k * width + z] = delta(pixels[i][j], pixels[k][z], alpha, theta)
 print('deltas ')
 
-# Construct matrix A in the linear system to be solved, where
-# A_ij = 2N if i = j, where N is the number of pixels in i's neighborhood
-# A_ij = -2 if j in N(i)
-# A_ij = 0  otherwise
+# Constrói a matriz A a ser resolvida no sistema linear
 diag = []
 for row in vizinhanca:
     for col in row:
@@ -125,49 +98,19 @@ for row in vizinhanca:
 A = np.diag(diag)
 for i in range(0, height):
     for j in range(0, width):
-        if (i - u) >= 0:
-            viz_cima = i - u
-        else:
-            viz_cima = 0
-        if (i + u) <= (height - 1):
-            viz_baixo = i + u
-        else:
-            viz_baixo = height - 1
-        if (j - u) >= 0:
-            viz_esq = j - u
-        else:
-            viz_esq = 0
-        if (j + u) <= (width - 1):
-            viz_dir = j + u
-        else:
-            viz_dir = width - 1
+        viz_cima, viz_baixo, viz_esq, viz_dir = calcula_limites(i, j, u, height, width)
         for k in range(viz_cima, viz_baixo+1):
             for z in range(viz_esq, viz_dir+1):
                 if i * width + j != k * width + z:
+                    # -2 indica que há uma influência negativa entre os pixels vizinhos na solução do sistema linear
                     A[i * width + j][k * width + z] = -2
 print('matriz a ')
 
-# Consturct vector b in the linear system to be solve, where
-# b_i = sum_{j in N(i)} (delta_ij - delta_ji)
+# Constrói o vetor B
 B = np.zeros((height * width,))
 for i in range(0, height):
     for j in range(0, width):
-        if (i - u) >= 0:
-            viz_cima = i - u
-        else:
-            viz_cima = 0
-        if (i + u) <= (height - 1):
-            viz_baixo = i + u
-        else:
-            viz_baixo = height - 1
-        if (j - u) >= 0:
-            viz_esq = j - u
-        else:
-            viz_esq = 0
-        if (j + u) <= (width - 1):
-            viz_dir = j + u
-        else:
-            viz_dir = width - 1
+        viz_cima, viz_baixo, viz_esq, viz_dir = calcula_limites(i, j, u, height, width)
         for k in range(viz_cima, viz_baixo+1):
             for z in range(viz_esq, viz_dir+1):
                 B[i * width + j] += deltas[i * width + j][k * width + z] - deltas[k * width + z][i * width + j]
@@ -178,11 +121,14 @@ g_flat = np.asarray([[pixels[row][col][0] for col in range(0,width)] for row in 
 
 plt.imshow(np.reshape(g_flat, (height, width)), cmap='gray', vmin=0, vmax=255)
 
+# ao resolver esse sistema, a solução será um vetor que representa como os valores de
+# delta devem ser ajustados para alcançar a conversão desejada de cor para escala de cinza
 res, info = cg(A, B, x0=g_flat)
 
 res = res + (l_media - res.mean())
 
-res = list(map(lambda x: x * 255 / 100, res))
+# escala de volta
+res = res * 255 / 100
 
 out = np.reshape(res,(height,width))
 
@@ -194,12 +140,6 @@ print("finished processing image", input_img,
       "output image to", out)
 cv2.imshow('output', out)
 
-
-
-# Exibir ou salvar os canais separados, se necessário
-# cv2.imshow('Canal L*', L)
-# cv2.imshow('Canal a*', a)
-# cv2.imshow('Canal b*', b)
 
 # Aguardar a tecla 'q' ser pressionada para fechar as janelas
 cv2.waitKey(0)
